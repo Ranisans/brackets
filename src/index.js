1ec48353d91865452b2f1901960fa601006d3e0c
Ranisans <ranisans@yahoo.com>
2019-09-30 15:47:10 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const identicalBrackets = findIdenticalBrackets(bracketsConfig);
+    const elementsArray = str.split("");
+    const elementStack = [];
+
+    elementsArray.forEach(element => {
+        bracketsConfig.forEach(pair => {
+            if (element === pair[0]) {
+                if (
+                    identicalBrackets.includes(element) &&
+                    elementStack[elementStack.length - 1] === element
+                ) {
+                    elementStack.pop();
+                } else {
+                    elementStack.push(element);
+                }
+            } else if (element === pair[1]) {
+                if (pair[0] === elementStack[elementStack.length - 1]) {
+                    elementStack.pop();
+                } else elementStack.push(element);
+            }
+        });
+    });
+
+    if (elementStack.length === 0) return true;
+    return false;
+};
+
+function findIdenticalBrackets(bracketsConfig) {
+    const identicalBrackets = [];
+    bracketsConfig.forEach(pair => {
+        if (pair[0] === pair[1]) identicalBrackets.push(pair[0]);
+    });
+
+    return identicalBrackets;
 }
